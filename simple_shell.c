42cf2e405a34b2e8d005194de7e423f6b70f4c4c
vigobin <vgobin@outlook.com>
2022-12-05 08:39:23 -0800

    Updated for betty style

--- simple_shell.c
+++ simple_shell.c
@@ -4,95 +4,102 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include "shell.h"
 
+/**
+ * shell_split_line - function which tokenizes string.
+ * @line: string input.
+ * Return: tokenized input.
+ */
 
-char** shell_split_line(char *line) 
+char **shell_split_line(char *line)
 {
-    int length = 0;
-    int capacity = 16;
-    char **tokens = malloc(capacity * sizeof(char*));
+	int length = 0;
+	int capacity = 16;
+	char **tokens = malloc(capacity * sizeof(char *));
 
-    char *delimiters = " \t\r\n";
-    char *token = strtok(line, delimiters);
+	char *delimiters = " \t\r\n";
+	char *token = strtok(line, delimiters);
 
-    while (token != NULL) 
-      {
-        tokens[length] = token;
-        length++;
-
-            if (length >= capacity) 
-       	       {
-
-	            capacity = (int) (capacity * 1.5);
-        	    tokens = realloc(tokens, capacity * sizeof(char*));
-              
-               } 
-
-           token = strtok(NULL, delimiters);
-       }
-
-    tokens[length] = NULL;
-    return tokens;
-}
-
-
-
-
-char* shell_read_line() 
-{
-    char *line = NULL;
-    size_t buflen = 0;
-    getline(&line, &buflen, stdin);
-    return line;
-}
-
-
-void shell_exec(char **args) 
-{     
-
-
-    pid_t child_pid = fork();
-
-    if (child_pid == 0) 
-      {
-        execve(args[0], args, NULL);
-        perror("Shell");
-        exit(1);
-      }
- 
-    else if (child_pid > 0)
-     	  {
-       	     int status;
-	     wait(&status);
-          }
- 
-     else
-        {
-
-        perror("Shell");
-
-        }
-}
-
-
-
-
-
-
-int main() 
-{
-    while (true) 
-    {
-        printf("$ ");
-        char *line = shell_read_line();
-        char **tokens = shell_split_line(line);
-
-        if (tokens[0] != NULL) 
+	while (token != NULL)
 	{
-            shell_exec(tokens);
-        }
+		tokens[length] = token;
+		length++;
 
-        free(tokens);
-        free(line);
-    }
+		if (length >= capacity)
+		{
+			capacity = (int) (capacity * 1.5);
+			tokens = realloc(tokens, capacity * sizeof(char *));
+		}
+		token = strtok(NULL, delimiters);
+	}
+
+	tokens[length] = NULL;
+	return (tokens);
+}
+
+/**
+ * shell_read_line - function which read the user input.
+ * Return: the user input.
+ */
+
+char *shell_read_line(void)
+{
+	char *line = NULL;
+	size_t buflen = 0;
+
+	getline(&line, &buflen, stdin);
+	return (line);
+}
+
+/**
+ * shell_exec - forks child processes.
+ * @args: commands being passed.
+ */
+
+void shell_exec(char **args)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid == 0)
+	{
+		execve(args[0], args, NULL);
+		perror("Shell");
+		exit(1);
+	}
+
+	else if (child_pid > 0)
+	{
+		int status;
+
+		wait(&status);
+	}
+
+	else
+	{
+		perror("Shell");
+	}
+}
+
+/**
+ * main - runs the shell.
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	while (true)
+	{
+		printf("$ ");
+		char *line = shell_read_line();
+		char **tokens = shell_split_line(line);
+
+		if (tokens[0] != NULL)
+		{
+			shell_exec(tokens);
+		}
+
+		free(tokens);
+		free(line);
+	}
 }
