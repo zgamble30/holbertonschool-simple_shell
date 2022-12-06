6fae7fc44f1844d1795d98aa0a988aa82d96e982
vigobin <vgobin@outlook.com>
2022-12-06 08:49:56 -0800

    included space in tokenizer

--- simple_shell.c
+++ simple_shell.c
@@ -12,13 +12,19 @@ char **shell_split_line(char *line)
 	int capacity = 16;
 	char **tokens = malloc(capacity * sizeof(char *));
 
-	char *delimiters = "\t\r\n";
+	char *delimiters = " \t\r\n";
 	char *token = strtok(line, delimiters);
 
 	while (token != NULL)
 	{
 		tokens[length] = token;
 		length++;
+
+		if (length >= capacity)
+		{
+			capacity = (int) (capacity * 1.5);
+			tokens = realloc(tokens, capacity * sizeof(char *));
+		}
 
 		token = strtok(NULL, delimiters);
 	}
