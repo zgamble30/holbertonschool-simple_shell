f85e566fa91eff6e0001401a3864e4a9d41327ad
Zach Gamble <zach@Zachs-MacBook-Pro.local>
2022-12-07 13:47:45 -0600

    added shell exec function

---
+++ shell_exec.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+ * shell_exec - forks child processes.
+ * @args: commands being passed.
+ */
+
+void shell_exec(char **args)
+{
+	pid_t child_pid = fork();
+
+	if (child_pid == 0)
+	{
+		execve(args[0], args, NULL);
+		perror("Error1");
+		exit(1);
+	}
+
+	else if (child_pid > 0)
+	{
+		int status;
+
+		wait(&status);
+	}
+
+	else
+	{
+		perror("Error2");
+	}
+}
