b6fbf07ef378533c331de54c4592996ed45f4012
vigobin <vgobin@outlook.com>
2022-12-08 07:33:46 -0800

    included exit statement

--- shell_exec.c
+++ shell_exec.c
@@ -6,7 +6,14 @@
 
 void shell_exec(char **args)
 {
-	pid_t child_pid = fork();
+	pid_t child_pid;
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		exit(0);
+	}
+
+	child_pid = fork();
 
 	if (child_pid == 0)
 	{
